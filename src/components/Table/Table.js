331fef6afded36c0c1d733d12ba8e87b57abfa81
Nabil Ahmed <105531667+nabil-github0@users.noreply.github.com>
2022-08-15 23:43:32 +0600

    Update Table.js

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -21,7 +21,7 @@ class Table extends Component {
 
   onButtonSubmit = () => {
     if(this.state.emailOrphone && this.state.password) {
-      fetch("http://localhost:3000/login", {
+      fetch("http://localhost:4000/login", {
         method: "post",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
